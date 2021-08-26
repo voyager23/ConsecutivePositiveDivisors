5739f090a3ca95c7ac77d798ba9e45ed042e1e4b
Mike <mike@nowhere>
2021-08-26 18:54:25 +0200

    restore point

--- dev/toolbox.hxx
+++ dev/toolbox.hxx
@@ -1,7 +1,7 @@
 /*
- * adev.cxx
+ * toolbox.hxx
  * 
- * Copyright 2021 Mike <mike@aorus>
+ * Copyright 2021 Michael Tate <mike@Michaels-MBP>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,41 +20,10 @@
  * 
  * 
  */
-
 #include <iostream>
-#include <vector>
+#include <numeric>
 #include <array>
-#include <iterator>
+#include <vector>
 
-#include "toolbox.hxx"
-
-typedef uint64_t ul;
-typedef struct {
-	ul n;	ul limit;		ul phi;
-}Data;
-
-int main(int argc, char **argv)
-{
-	const ul Limit = 16;
-	std::vector<Data> data;
-	std::vector<Data>::iterator a,w;
-	// setup
-	for(ul x = 1; x <= Limit; ++x) data.push_back({x, (x/2)+1, 0});
-	a = std::next(data.begin(),1);
-	while(a < data.end()){
-		//
-		w = a;
-		while(w < data.end()){
-			if( (a->n <= w->limit) and ( (w->n % a->n) == 0) ) w->phi += 1;
-			std::advance(w, a->n);
-		}
-		
-		std::advance(a,1);
-		
-		// debug print
-		for (auto x = data.begin()+1; x != data.end(); ++x) printf("n:%llu limit:%llu phi:%llu\n", x->n, x->limit, x->phi);
-		printf("a advanced to %llu\n", a->n);
-	}
-	return 0;
-}
+void SieveOfEratosthenes(std::vector<int> &primes,int n);
 
